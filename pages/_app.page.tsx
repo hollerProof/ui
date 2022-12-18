150e016a50e1096e4f717426245a9ec82f034d30
0xdenominator <zeroxdenominator@gmail.com>
2022-12-18 11:13:55 +0100

    :sparkles: Add analytics

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
   <Component {...pageProps} />
-    </ChakraProvider>
+    <Analytics />
+  </ChakraProvider>
 }
 
 export default MyApp
